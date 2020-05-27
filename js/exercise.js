f0f3cf09f78bdf39c36b3784ad1b29f96c00c22f
OlegTripolskiy <bizprosto@gmail.com>
2020-05-27 20:38:54 +0300

    0.0.0.3.4  DRAG & DROP изменил курсор и сдвинул предлог относительно курсора.

--- js/exercise.js
+++ js/exercise.js
@@ -1,44 +1,14 @@
 // === УПРАЖНЕНИЯ по АНГЛИЙСКОМУ ЯЗЫКУ ==========
-$("document").ready(function () {
-    /*
-    $(document).on("touchstart  mousedown", ".prep", function(event) {
-            //если касание, то вычисляем через event.originalEvent.touches[0]:
-            if (event.type == "touchstart") {
-              alert("TouchSTART");
-                //var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
-                //var offset = (touch.clientX - $(event.target).offset().left);
-            }
-            else {
-            //если нажата кнопка мышки:
-              let = 
-              console.log("Mouse DOWN");
-              //alert("Mouse DOWN");
-                //var offset = (event.offsetX || event.clientX - $(event.target).offset().left);
-            }
-            //console.log(offset);
-            //отменяем "всплытие сообщений", чтобы не вызывался клик на тач-устройствах.
-            event.stopPropagation();
-            event.preventDefault();
-});
-*/  /*
-    // --- проба события tachstart - РАБОТАЕТ.
-    $(".preposiotions").on('click', '.prep', function(){
-      $(this).addClass('circle');
-    });
-    //$(".prep").addEventListener("click", go());
-    */
+//  --- Для сенсорных экранов ВЫДЕЛЯЕМ предлог зеленым кругом. ---
+$("document").ready(function () {  // Добввяем класс при прикосновении
     $(document).on("touchstart", ".prep", function(event) {
-      //alert("TouchSTART");
-      $(this).addClass('circle');
+        $(this).addClass('circle');
     });
-    $(document).on("touchend", ".prep", function(event) {
-      //alert("TouchEND");
-      $(this).removeClass('circle');
-    });  
-    //$( ".prep" ).addEventListener('touchstart', function(){
-    //  $(this).addClass('circle');
-    //});
-  
+
+    $(document).on("touchend", ".prep", function(event) {  // Удаляем класс при снятии пальца.
+        $(this).removeClass('circle');
+    });
+
   // --- проба draggable --------------
     $( ".prep" ).draggable({
         containment:"#exercise-1", revert: true, revertDuration: 0,
