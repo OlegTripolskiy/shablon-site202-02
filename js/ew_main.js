dc2ce86f048d1deaef4691fd7793885c156badf3
OlegTripolskiy <bizprosto@gmail.com>
2020-06-05 13:44:57 +0300

    0.0.0.3.6  SORTABLE – сортировка таблицы.

--- js/ew_main.js
+++ js/ew_main.js
@@ -53,11 +53,15 @@ $(document).ready(function () {
       // --- КОНТРОЛЬ СОБЫТИЙ (DESKTOP или SENSITIVE)
     // --- ПРОБА обработки событий с помощью switch case  
     $('.tr').on('touchend', function(event){ // Отслеживаем СРАЗУ ДВА события.
-        alert(" Сработало - " + event.type );
+        //alert(" Сработало - " + event.type );
         $('.tr').off('mouseover');
+        $('.tr').removeClass('show'); // Удаляем у всех ПОКАЗ псевдоклассов.
+        $(this).addClass("show");     // Показываем псевдоклассы только у текущего
     });
     $('.tr').on('mouseover', function(event){
-        alert(" Сработало - " + event.type );
+        //alert(" Сработало - " + event.type );
+        $('.tr').removeClass('show'); // Удаляем у всех ПОКАЗ псевдоклассов.
+        $(this).addClass("show");     // Показываем псевдоклассы только у текущего
     });
     
    /* 
